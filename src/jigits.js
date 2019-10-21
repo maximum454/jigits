9ce11052c11f0b30abfbf6da0b62838b7df29776
maxim <maximum454@gmail.com>
2019-10-21 11:41:54 +0600

    fix

--- src/jigits.js
+++ src/jigits.js
@@ -1,4 +1,7 @@
 import './jigits.scss'
+import './scss/patrial/slick.scss'
+import './js/slick'
+
 
 import './img/logo.svg'
 import './img/salons.jpg'
@@ -8,3 +11,25 @@ import './img/barbers-1.jpg'
 import './img/barbers-2.jpg'
 import './img/MorgansLogo.svg'
 import './img/footer-instagram.svg'
+import './img/99907.jpg'
+import './img/102373.jpg'
+import './img/102694.jpg'
+import './img/104929.jpg'
+import './img/DSC00617.jpg'
+import './img/DSC00649.jpg'
+import './img/DSC00709.jpg'
+
+import './img/DSC00825.jpg'
+import './img/DSC00866.jpg'
+import './img/DSC00891.jpg'
+
+
+$('.js-slider').slick({
+    dots: true,
+    autoplay:true,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+});
